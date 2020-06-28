f9db922791c148e1c0260d50270fe1984f67e700
KrystynaGrzesiak <krygrz11@gmail.com>
2020-06-29 00:44:00 +0200

    updates memhdx model

--- R/memhdx_model.R
+++ R/memhdx_model.R
@@ -1,23 +1,52 @@
 library(lme4)
 library(lmerTest)
 
-jurgen_model = function(data, compare = "Deuteration", alpha = 0.05) {
-  if (compare != "Mass" & compare != "Deuteration") stop("Either Mass or Deuteration must be chosen.")
+jurgen_model = function(data, significance_level = 0.05) {
   
-  data = calculate_deuteration(data, compare)
+  States = unique(data$State)
   
+  Time = c("continuous", "categorical", "continuous")
+  Transformation = c("identity", "identity", "log")
+  
+  aic = rep(NA, 3)
+  loglik = rep(NA, 3)
+  Test_statistic = rep(NA, 3)
+  p_value = rep(NA, 3)
+  
+  # continuous, identity
   model = lmer(Mass ~ State + Exposure*State + Exposure + (1|Rep), data = data)
   result = anova(model)
-  p_value = result$`F value`[1]
+  aic[1] = AIC(model)
+  loglik[1] = logLik(model)
+  Test_statistic[1] = result$`F value`[1]
+  p_value[1] = result$`Pr(>F)`[1]
   
-  if (p_value <= alpha) {
-    conclusion = "different"
-  } else {
-    conclusion = "equal"
-  }
   
-  data.frame(Test = "model MEMHDH", #TODO: name model
-             Covariate = compare,
-             p_value = p_value,
-             conclusion = conclusion)
+  # categorical, identity
+  model = lmer(Mass ~ State + factor(Exposure)*State + factor(Exposure) + (1|Rep), data = data)
+  result = anova(model)
+  aic[2] = AIC(model)
+  loglik[2] = logLik(model)
+  Test_statistic[2] = result$`F value`[1]
+  p_value[2] = result$`Pr(>F)`[1]
+  
+  # continuous, log
+  model = lmer(log(Mass+1) ~ State + Exposure*State + Exposure + (1|Rep), data = data)
+  result = anova(model)
+  aic[3] = AIC(model)
+  loglik[3] = logLik(model)
+  Test_statistic[3] = result$`F value`[1]
+  p_value[3] = result$`Pr(>F)`[1]
+  
+  
+  data.table(Test = "MEMHDX lmm",
+             State_1 = States[1],
+             State_2 = States[2],
+             F_statistic = F_statistic,
+             P_value = p_value,
+             Significant_difference = (p_value <= significance_level),
+             Time = Time,
+             Transformation = Transformation,
+             AIC = aic,
+             logLik = loglik)
 }
