2937e4132c4aa1af48c8fb3089f750a3a476bed0
KrystynaGrzesiak <krygrz11@gmail.com>
2020-06-28 23:57:57 +0200

    changes deuteros

--- R/simplest_model.R
+++ R/simplest_model.R
@@ -1,21 +1,43 @@
-simplest_model = function(data, compare = "Deuteration", alpha = 0.05) {
-  if (compare != "Mass" & compare != "Deuteration") stop("Either Mass or Deuteration must be chosen.")
+
+
+deuteros = function(data, significance_level = 0.05) {
   
   data = calculate_deuteration(data, compare)
   
+  Time = c("continuous", "categorical", "continuous")
+  Transformation = c("identity", "identity", "log")
+  
+  aic = rep(NA, 3)
+  loglik = rep(NA, 3)
+  p_value = rep(NA, 3)
+  
+  # continuous, identity
   model = lm(Mass ~ Exposure*State, data = data)
   result = anova(model)
+  aic[1] = AIC(model)
+  loglik[1] = logLik(model)
+  p_value[1] = result$`Pr(>F)`[2]
   
-  p_value = result$`Pr(>F)`[2]
+  # categorical, identity
+  model = lm(Mass ~ factor(Exposure)*State, data = data)
+  result = anova(model)
+  aic[2] = AIC(model)
+  loglik[2] = logLik(model)
+  p_value[2] = result$`Pr(>F)`[2]
   
-  if (p_value <= alpha) {
-    conclusion = "different"
-  } else {
-    conclusion = "equal"
-  }
+  # continuous, log
+  model = lm(log(Mass) ~ Exposure*State, data = data)
+  result = anova(model)
+  aic[3] = AIC(model)
+  loglik[3] = logLik(model)
+  p_value[3] = result$`Pr(>F)`[2]
   
-  data.frame(Test = "model 1", #TODO: name model
-             Covariate = compare,
-             p_value = p_value,
-             conclusion = conclusion)
+  data.table(Test = "Deuteros", #TODO: name model
+             P_value = p_value,
+             Significant_difference = (p_value <= alpha),
+             Time = Time,
+             Transformation = Transformation,
+             AIC = aic,
+             logLik = loglik)
 }
+
