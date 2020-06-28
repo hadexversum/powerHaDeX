d487f446325b4eebfb2b61515f20698336412b30
KrystynaGrzesiak <krygrz11@gmail.com>
2020-06-29 00:40:40 +0200

    adds states and F statistic

--- R/simplest_model.R
+++ R/simplest_model.R
@@ -1,15 +1,18 @@
-#' Simplest linear models for deuteration curves
+#' Simplest linear models for deuteration curves containing time, state of the protein and the interaction term,.
 #' @param data data.table with deuteration curves
 #' @param significance_level significance level for tests
 #' @importFrom data.table data.table
 #' @export
 deuteros = function(data, significance_level = 0.05) {
+  
+  States = unique(data$State)
 
   Time = c("continuous", "categorical", "continuous")
   Transformation = c("identity", "identity", "log")
 
   aic = rep(NA, 3)
   loglik = rep(NA, 3)
+  F_statistic = rep(NA, 3)
   p_value = rep(NA, 3)
 
   # continuous, identity
@@ -17,6 +20,7 @@ deuteros = function(data, significance_level = 0.05) {
   result = anova(model)
   aic[1] = AIC(model)
   loglik[1] = logLik(model)
+  F_statistic[1] = result$`F value`[2]
   p_value[1] = result$`Pr(>F)`[2]
 
   # categorical, identity
@@ -24,6 +28,7 @@ deuteros = function(data, significance_level = 0.05) {
   result = anova(model)
   aic[2] = AIC(model)
   loglik[2] = logLik(model)
+  F_statistic[2] = result$`F value`[2]
   p_value[2] = result$`Pr(>F)`[2]
 
   # continuous, log
@@ -31,9 +36,13 @@ deuteros = function(data, significance_level = 0.05) {
   result = anova(model)
   aic[3] = AIC(model)
   loglik[3] = logLik(model)
+  F_statistic[3] = result$`F value`[2]
   p_value[3] = result$`Pr(>F)`[2]
 
-  data.table::data.table(Test = "Deuteros", #TODO: name model
+  data.table::data.table(Test = "Deuteros lm",
+                         State_1 = States[1],
+                         State_2 = States[2],
+                         F_statistic = F_statistic,
                          P_value = p_value,
                          Significant_difference = (p_value <= significance_level),
                          Time = Time,
