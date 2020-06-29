1b7110c6e3ab15cd7adef570ea219c904131dff8
KrystynaGrzesiak <krygrz11@gmail.com>
2020-06-29 02:14:34 +0200

    adds test for nested models

--- R/simplest_model.R
+++ R/simplest_model.R
@@ -17,26 +17,29 @@ deuteros = function(data, significance_level = 0.05) {
 
   # continuous, identity
   model = lm(Mass ~ Exposure*State, data = data)
-  result = anova(model)
+  model_reduced = lm(Mass ~ Exposure, data = data)
+  result = anova(model, model_reduced)
   aic[1] = AIC(model)
   loglik[1] = logLik(model)
-  F_statistic[1] = result$`F value`[2]
+  F_statistic[1] = result$`F`[2]
   p_value[1] = result$`Pr(>F)`[2]
 
   # categorical, identity
   model = lm(Mass ~ factor(Exposure)*State, data = data)
-  result = anova(model)
+  model_reduced = lm(Mass ~ factor(Exposure), data = data)
+  result = anova(model, model_reduced)
   aic[2] = AIC(model)
   loglik[2] = logLik(model)
-  F_statistic[2] = result$`F value`[2]
+  F_statistic[2] = result$`F`[2]
   p_value[2] = result$`Pr(>F)`[2]
 
   # continuous, log
   model = lm(Mass ~ log(Exposure + 1)*State, data = data)
-  result = anova(model)
+  model_reduced = lm(Mass ~ log(Exposure + 1), data = data)
+  result = anova(model, model_reduced)
   aic[3] = AIC(model)
   loglik[3] = logLik(model)
-  F_statistic[3] = result$`F value`[2]
+  F_statistic[3] = result$`F`[2]
   p_value[3] = result$`Pr(>F)`[2]
 
   data.table::data.table(Test = "Deuteros lm",
