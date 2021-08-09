a3d87ca6e3612f9ccdc7fb675fa0f1010407333b
Krysia <krysiagrzesiak11@wp.pl>
2021-08-09 11:48:12 +0200

    rewrites tests

--- tests/testthat/test_exchange_rates.R
+++ tests/testthat/test_exchange_rates.R
@@ -1,33 +1,20 @@
 
 #get_F_const
-test_that("get_F_const returns proper output", {
-    gas_constant = 1.9858775
-    temp_kelvin = 291.15
-    F_const <- get_F_const(temp_kelvin, gas_constant)
-
-    expect_true(typeof(F_const) == "list")
-    expect_true(length(F_const) == 3)
-    expect_equal(unname(sapply(F_const, class)), c("numeric", "numeric", "numeric"))
-})
-
-
 test_that("get_F_const works", {
-    gas_constant = 1.9858775
-
-    temp_kelvin = 291.15
-    F_const <- as.numeric(unlist(get_F_const(temp_kelvin, gas_constant)))
-    expect_true(all.equal(F_const, c(0.8582290117, 0.8305682088, 0.81262475)))
-
-    temp_kelvin = 300
-    F_const <- get_F_const(temp_kelvin, gas_constant)
-    expect_equal(as.numeric(unlist(F_const)), c(1.753153117, 1.977274469, 2.142389176))
+    F_const <- get_F_const(291.15, 1.9858775)
+    expect_equal(F_const, list(Fta = 0.858229011688035,
+                               Ftb = 0.830568208785807,
+                               Ftw = 0.812624750043961))
+    F_const <- get_F_const(300, 1.9858775)
+    expect_equal(F_const, list(Fta = 1.75315311725701,
+                               Ftb = 1.97727446855674,
+                               Ftw = 2.14238917600377))
 })
 
 test_that("get_F_const returns error", {
-    gas_constant = 1.9858775
-    temp_kelvin = 0
-    expect_error(get_F_const(temp_kelvin, gas_constant))
+    expect_error(get_F_const(0, 1.9858775))
 })
+
 
 #get_poly_const
 test_that("get_poly_const returns error for invalid input", {
@@ -38,60 +25,26 @@ test_that("get_poly_const returns error for invalid input", {
     expect_error(get_poly_const("oligo", "poly"))
 })
 
-
-test_that("get_poly_const returns proper output", {
+test_that("get_poly_const works", {
     #exchange HD
     poly_const = get_poly_const("poly", "HD")
+    expect_equal(poly_const, list(Ka = 0.694782305783893,
+                                  Kb = 187003075.716994,
+                                  Kw = 0.00052704627669473))
     oligo_const = get_poly_const("oligo", "HD")
-
-    expect_true(typeof(poly_const) == "list")
-    expect_true(length(poly_const) == 3)
-    expect_equal(unname(sapply(poly_const, class)), c("numeric", "numeric", "numeric"))
-
-    expect_true(typeof(oligo_const) == "list")
-    expect_true(length(oligo_const) == 3)
-    expect_equal(unname(sapply(oligo_const, class)), c("numeric", "numeric", "numeric"))
-
-    expect_equal(names(poly_const), c("Ka", "Kb", "Kw"))
-    expect_equal(names(oligo_const), c("Ka", "Kb", "Kw"))
+    expect_equal(oligo_const, list(Ka = 1.62579059553431,
+                                   Kb = 252454152.217942,
+                                   Kw = 0.000835368348561147))
 
     #exchange DH
     poly_const = get_poly_const("poly", "DH")
+    expect_equal(poly_const, list(Ka = 0.41864773858493,
+                                  Kb = 123551706.883486,
+                                  Kw = 0.00041864773858493))
     oligo_const = get_poly_const("oligo", "DH")
-
-    expect_true(typeof(poly_const) == "list")
-    expect_true(length(poly_const) == 3)
-    expect_equal(unname(sapply(poly_const, class)), c("numeric", "numeric", "numeric"))
-
-    expect_true(typeof(oligo_const) == "list")
-    expect_true(length(oligo_const) == 3)
-    expect_equal(unname(sapply(oligo_const, class)), c("numeric", "numeric", "numeric"))
-
-    expect_equal(names(poly_const), c("Ka", "Kb", "Kw"))
-    expect_equal(names(oligo_const), c("Ka", "Kb", "Kw"))
-})
-
-
-test_that("get_poly_const works", {
-    #HD
-    poly_const = get_poly_const("poly", "HD")
-    oligo_const = get_poly_const("oligo", "HD")
-
-    poly_const = as.numeric(unlist(poly_const))
-    oligo_const = as.numeric(unlist(oligo_const))
-
-    expect_equal(poly_const, c(0.6947823, 187003076, 0.0005270463))
-    expect_equal(oligo_const, c(1.625791, 252454152, 0.0008353683))
-
-    #DH
-    poly_const = get_poly_const("poly", "DH")
-    oligo_const = get_poly_const("oligo", "DH")
-
-    poly_const = as.numeric(unlist(poly_const))
-    oligo_const = as.numeric(unlist(oligo_const))
-
-    expect_equal(poly_const, c(0.4186477, 123551707, 0.0004186477))
-    expect_equal(oligo_const, c(0.9796357, 166794804, 0.0006635567))
+    expect_equal(oligo_const, list(Ka = 0.979635708288736,
+                                   Kb = 166794804.292706,
+                                   Kw = 0.000663556665657114))
 })
 
 
@@ -101,25 +54,14 @@ test_that("get_pkc returns proper output", {
     temp_kelvin = 291.15
     #HD
     pkc_const_HD = get_pkc(temp_kelvin, gas_constant, exchange = "HD")
-    expect_true(typeof(pkc_const_HD) == "list")
-    expect_true(length(pkc_const_HD) == 3)
-    expect_equal(unname(sapply(pkc_const_HD, class)), c("numeric", "numeric", "numeric"))
-    expect_equal(names(pkc_const_HD), c("asp", "glu", "his"))
+    expect_equal(pkc_const_HD, list(asp = 4.44446993365174,
+                                    glu = 4.89152093814484,
+                                    his = 7.15352450238806))
     #DH
     pkc_const_DH = get_pkc(temp_kelvin, gas_constant, exchange = "DH")
-    expect_true(typeof(pkc_const_DH) == "list")
-    expect_true(length(pkc_const_DH) == 3)
-    expect_equal(unname(sapply(pkc_const_DH, class)), c("numeric", "numeric", "numeric"))
-    expect_equal(names(pkc_const_DH), c("asp", "glu", "his"))
-})
-
-
-test_that("get_pkc works", {
-
-    expect_equal(as.numeric(unlist(get_pkc(300, 1.9858775, "HD"))),
-                 c(4.422311601, 4.867523464, 6.987337008))
-    expect_equal(as.numeric(unlist(get_pkc(300, 1.9858775, "DH"))),
-                 c(3.814619137, 4.267523464, 6.567337008))
+    expect_equal(pkc_const_DH, list(asp = 3.83589113630567,
+                                    glu = 4.29152093814484,
+                                    his = 6.73352450238806))
 })
 
 test_that("get_pkc returns error in the case of invalid output", {
@@ -139,55 +81,25 @@ test_that("get_pkc returns proper output", {
                     Kw = 0.0005270462767)
 
     constants = get_exchange_constants(pH, pkc_consts, k_consts)
-    expect_equal(dim(constants), c(28, 4))
-    expect_equal(class(constants), c("matrix", "array"))
-    expect_equal(typeof(constants), "double")
-    expect_false(all(is.na(constants)))
+    expect_equal(constants, structure(c(0, -0.59, 0.898812310601082, -0.9, -0.58, -0.54,
+                                        -0.74, -0.22, -0.896671812875742, -0.6, -0.47, -0.296158254546754,
+                                        -0.91, -0.57, -0.56, -0.64, -0.52, 0, 0, -0.437992277698594,
+                                        -0.79, -0.4, -0.41, -0.739022273362575, 0, 0.957046086760691,
+                                        0.511939295200356, 0, 0, -0.32, 0.57903422200311, -0.12, -0.13,
+                                        -0.46, -0.58, 0.218176047120386, 0.307518999008732, -0.27, -0.27,
+                                        -0.22615670112472, -0.59, -0.13, -0.29, -0.28, -0.43, -0.194773472023435,
+                                        -0.854416534276379, -0.388518934646472, -0.468073125742265, -0.44,
+                                        -0.37, -0.3, -1.32, 0, 0, 0.293, 0, 0.0767122542818456, -0.289554452431933,
+                                        0.69, 0.49, 0.62, 0.55, 0.267251569286023, -0.494750659376026,
+                                        0.24, 0.06, 0.605773780975473, -0.73, -0.576252727721677, -0.04,
+                                        -0.00895484265292644, -0.235859464059171, 0, 0, 0.37, -0.0662579798400606,
+                                        -0.41, -0.27, -0.701934483299758, 0, -1.8, -0.577243550643321,
+                                        0, 0, 0.22, -0.173985160086455, 0.6, 0.32, 0.55, 0.46, 0.17,
+                                        -0.141795340523512, 0.39, 0.2, 0.657172312134108, -0.23, -0.21,
+                                        0.12, 0.11, 0.0631315866300978, -0.24, 0.6, 0.299550285605933,
+                                        0.2, -0.11, 0.05, -0.14, 1.62, 0, 0, -0.197), .Dim = c(28L, 4L
+                                        )))
 })
-
-
-test_that("get_exchange_constants works", {
-    pH = 7
-    pkc_consts = list(asp = 4.444469934,
-                      glu = 4.891520938,
-                      his = 7.153524502)
-    k_consts = list(Ka = 0.6947823058,
-                    Kb = 187003075.7,
-                    Kw = 0.0005270462767)
-
-    constants = matrix(c(0.0000000000,  0.0000000000,  0.000000000000,  0.00000000000,
-                         -0.5900000000, -0.3200000000,  0.076712254282,  0.22000000000,
-                         0.8988123106,  0.5790342220, -0.289554452432, -0.17398516009,
-                         -0.9000000000, -0.1200000000,  0.690000000000,  0.60000000000,
-                         -0.5800000000, -0.1300000000,  0.490000000000,  0.32000000000,
-                         -0.5400000000, -0.4600000000,  0.620000000000,  0.55000000000,
-                         -0.7400000000, -0.5800000000,  0.550000000000, 0.46000000000,
-                         -0.2200000000,  0.2181760471,  0.267251569286, 0.17000000000,
-                         -0.8966718129,  0.3075189990, -0.494750659376, -0.14179534052,
-                         -0.6000000000, -0.2700000000,  0.240000000000,  0.39000000000,
-                         -0.4700000000, -0.2700000000,  0.060000000000,  0.20000000000,
-                         -0.2961582545, -0.2261567011,  0.605773780975,  0.65717231213,
-                         -0.9100000000, -0.5900000000, -0.730000000000, -0.23000000000,
-                         -0.5700000000, -0.1300000000, -0.576252727722, -0.21000000000,
-                         -0.5600000000, -0.2900000000, -0.040000000000,  0.12000000000,
-                         -0.6400000000, -0.2800000000, -0.008954842653,  0.11000000000,
-                         -0.5200000000, -0.4300000000, -0.235859464059,  0.06313158663,
-                         0.0000000000, -0.1947734720,  0.000000000000, -0.24000000000,
-                         0.0000000000, -0.8544165343,  0.000000000000,  0.60000000000,
-                         -0.4379922777, -0.3885189346,  0.370000000000,  0.29955028561,
-                         -0.7900000000, -0.4680731257, -0.066257979840,  0.20000000000,
-                         -0.4000000000, -0.4400000000, -0.410000000000, -0.11000000000,
-                         -0.4100000000, -0.3700000000, -0.270000000000,  0.05000000000,
-                         -0.7390222734, -0.3000000000, -0.701934483300, -0.14000000000,
-                         0.0000000000, -1.3200000000, 0.000000000000,  1.62000000000,
-                         0.9570460868,  0.0000000000, -1.800000000000,  0.00000000000,
-                         0.5119392952,  0.0000000000, -0.577243550643,  0.00000000000,
-                         0.0000000000,  0.2930000000,  0.000000000000, -0.19700000000),
-                       byrow = TRUE, ncol = 4)
-
-    expect_equal(get_exchange_constants(pH, pkc_consts, k_consts), constants)
-})
-
 
 
 #get_exchange_rates
@@ -198,14 +110,12 @@ test_that("get_exchange_rates returns proper output", {
     if_corr = 0
 
     kcHD = get_exchange_rates(sequence, exchange = "HD", mol_type = "poly")
-    expect_true(length(kcHD) == 6)
-    expect_true(class(kcHD) == "numeric")
-    expect_true(typeof(kcHD) == "double")
+    expect_equal(kcHD, c(0, 0, 58.6518885939027, 117.025903012734, 142.605325751056,
+                         0.441180572094274))
 
     kcDH = get_exchange_rates(sequence, exchange = "DH", mol_type = "poly")
-    expect_true(length(kcDH) == 6)
-    expect_true(class(kcDH) == "numeric")
-    expect_true(typeof(kcDH) == "double")
+    expect_equal(kcDH, c(0, 0, 293.955039643536, 586.51741289567, 670.603937240368,
+                         2.13099882731867))
 })
 
 
@@ -219,18 +129,3 @@ test_that("get_exchange_rates returns error in the case of invalid output", {
     expect_error(get_exchange_rates(sequence, exchange = "DH", mol_type = "blahblah"))
     expect_error(get_exchange_rates(sequence, if_corr = 17))
 })
-
-
-test_that("get_exchange_rates works", {
-    sequence = c("P", "P", "A", "Q", "H", "I")
-
-    get_exchange_rates(sequence)
-
-    expect_equal(get_exchange_rates(sequence),
-                 c(0, 0, 58.6518885939, 117.0259030127, 142.6053257511, 0.4411805721))
-
-    expect_equal(get_exchange_rates(sequence, exchange = "DH"),
-                 c(0, 0, 293.955039644, 586.517412896, 670.603937240, 2.130998827))
-
-})
-
