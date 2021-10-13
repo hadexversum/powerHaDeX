019be9e46438e3103681d27b6f61fdcfba8416f0
KrystynaGrzesiak <krygrz11@gmail.com>
2021-10-13 13:51:31 +0200

    adds more tests

---
+++ tests/testthat/test_create_experimental_file.R
@@ -0,0 +1,21 @@
+
+test_that("experimental file is created", {
+    set.seed(44)
+
+    peptides <- data.frame(sequence = c("FPTTKTY", "LVRKDLQN"),
+                           protection_factor = c(10, 100))
+    res <- create_experimental_file(peptides, charge = 1:3)
+
+    expected <- readRDS("experimental_file.RDS")
+    expect_equal(res, expected)
+})
+
+
+test_that("experimental returns error", {
+    set.seed(44)
+
+    peptides <- data.frame()
+    expect_error(create_experimental_file(peptides, charge = 1:3),
+                 "You must provide at least one sequence to simulate spectra.
+                                              See simulate_theoretical_spectra for more details.")
+})
